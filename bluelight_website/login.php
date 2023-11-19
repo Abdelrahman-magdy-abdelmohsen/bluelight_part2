ad53827795f7622d94742410894dc4a31506edc0
Abdelrahman magdy <abdelrahman.ahmed2@hims.edu.eg>
2023-11-19 23:58:56 +0200

    Add files via upload

---
+++ bluelight_website/login.php
@@ -0,0 +1,27 @@
+<?php
+include_once "header.php";
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>login</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+
+<div class="container d-flex justify-content-center mt-5 mb-5">
+
+<div class="login_con m-auto">
+<h3 class="text-center"> login </h3>
+    <i class="fa-solid fa-user"></i>
+</div>
+</div>
+</body>
+</html>
